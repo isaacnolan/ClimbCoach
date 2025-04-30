c58f8adb985239f3007d2376a3950735d2304247
isaacnolan <isaac.t.nolan@gmail.com>
2025-04-29 20:55:44 -0400

    Simple LangChain experiments

--- backend/test.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-openai.api_key = "sk-proj-1234567890"
-
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Hello, how are you?"}])
-
-print(response)
